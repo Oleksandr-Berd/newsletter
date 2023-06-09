027441887d2fa840907793fcc0104dd520fb28cf
Oleksandr-Berd <alex.berd86@gmail.com>
2023-06-09 09:54:43 +0300

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter>
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
